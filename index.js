8e3ebee644d7b0bb136beb45b69c2ad196c6d5aa
fahim <muhdfahim786@gamil.com>
2026-04-17 21:42:19 +0530

    create signup API route

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config()
 import userRouter from "./routes/user.routes.js"
+import authRouter from "./routes/auth.routes.js"
 
 mongoose.connect(process.env.MONGO).then(()=>console.log("connected to Mongodb"))
 .catch(err=>console.log(err));
 
 
 const app=express()
+app.use(express.json())
 
 app.get("/", (req, res) => {
   console.log("🔥 route working");
@@ -17,6 +19,7 @@ app.get("/", (req, res) => {
   });
 });
 app.use("/api/user",userRouter)
+app.use("/api/auth",authRouter)
 
 app.listen(process.env.PORT,()=>{
     console.log(`server running on port ${process.env.PORT} `);
